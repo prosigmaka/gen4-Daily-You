fc8e6d74568dadcfcb49b575dc0e4bbe79cf2b6d
Intan Farwati Amatillah <intanfarwati@gmail.com>
2021-04-20 10:21:30 +0700

    push one

---
+++ src/main/java/com/kelompok1/dailyyou/DailyYouApplication.java
@@ -0,0 +1,13 @@
+package com.kelompok1.dailyyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyYouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyYouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kelompok1/dailyyou/DailyYouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kelompok1.dailyyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyYouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
