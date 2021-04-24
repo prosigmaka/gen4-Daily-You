882df9a98a29f143451f92c3ff230d07aaee7ec0
Fadhilah Tri Larasaty <fadhilahlaras97@gmail.com>
2021-04-24 14:09:43 +0700

    Update Java and DTO File Names

--- src/main/java/com/kelompok1/dailyyou/model/entity/AddressCategory.java
+++ src/main/java/com/kelompok1/dailyyou/model/entity/AddressCategory.java
@@ -5,9 +5,9 @@ import lombok.Data;
 import javax.persistence.*;
 
 @Entity
-@Table(name=KategoriAlamat.TABLE_NAME)
+@Table(name= AddressCategory.TABLE_NAME)
 @Data
-public class KategoriAlamat {
+public class AddressCategory {
     public static final String TABLE_NAME = "t_kategori_alamat";
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = TABLE_NAME)
