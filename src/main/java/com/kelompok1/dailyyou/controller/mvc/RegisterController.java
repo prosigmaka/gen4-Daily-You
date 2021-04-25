3acafec8f61fd0f81b0dcfab9e887e7ccf2cf256
Fadhilah Tri Larasaty <fadhilahlaras97@gmail.com>
2021-04-25 16:40:14 +0700

    Upload Controller dan HTML Register

---
+++ src/main/java/com/kelompok1/dailyyou/controller/mvc/RegisterController.java
@@ -0,0 +1,32 @@
+package com.kelompok1.dailyyou.controller.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class RegisterController {
+    @GetMapping(value = "/register")
+    public ModelAndView register(
+            @RequestParam(value = "error", required = false) String error,
+            @RequestParam(value = "logout", required = false) String logout,
+            @RequestParam(value = "denied", required = false) String denied) {
+
+        ModelAndView model = new ModelAndView();
+        if (error != null) {
+            model.addObject("msg", "Invalid username or password!");
+        }
+
+        if (logout != null) {
+            model.addObject("msg", "You've been logged out successfully.");
+        }
+
+        if (denied != null) {
+            model.addObject("msg", "Access Denied. Session Time Out");
+        }
+        model.setViewName("register");
+
+        return model;
+    }
+}
