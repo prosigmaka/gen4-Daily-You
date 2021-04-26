aed7abd3a53f7d48e3c860a3e5334add8ead2f59
Fadhilah Tri Larasaty <fadhilahlaras97@gmail.com>
2021-04-26 10:47:56 +0700

    Update Register Controller

--- src/main/java/com/kelompok1/dailyyou/controller/mvc/RegisterController.java
+++ src/main/java/com/kelompok1/dailyyou/controller/mvc/RegisterController.java
@@ -7,26 +7,33 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class RegisterController {
-    @GetMapping(value = "/register")
-    public ModelAndView register(
-            @RequestParam(value = "error", required = false) String error,
-            @RequestParam(value = "logout", required = false) String logout,
-            @RequestParam(value = "denied", required = false) String denied) {
 
-        ModelAndView model = new ModelAndView();
-        if (error != null) {
-            model.addObject("msg", "Invalid username or password!");
-        }
-
-        if (logout != null) {
-            model.addObject("msg", "You've been logged out successfully.");
-        }
-
-        if (denied != null) {
-            model.addObject("msg", "Access Denied. Session Time Out");
-        }
-        model.setViewName("register");
-
-        return model;
+    //    dashboard
+    @GetMapping("register")
+    public String dashboard() {
+        return "dashboard/index";
     }
+
+//    @GetMapping(value = "/register")
+//    public ModelAndView register(
+//            @RequestParam(value = "error", required = false) String error,
+//            @RequestParam(value = "logout", required = false) String logout,
+//            @RequestParam(value = "denied", required = false) String denied) {
+//
+//        ModelAndView model = new ModelAndView();
+//        if (error != null) {
+//            model.addObject("msg", "Invalid username or password!");
+//        }
+//
+//        if (logout != null) {
+//            model.addObject("msg", "You've been logged out successfully.");
+//        }
+//
+//        if (denied != null) {
+//            model.addObject("msg", "Access Denied. Session Time Out");
+//        }
+//        model.setViewName("register");
+//
+//        return model;
+//    }
 }
