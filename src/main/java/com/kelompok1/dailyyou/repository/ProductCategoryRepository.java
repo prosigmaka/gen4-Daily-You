b9d6ed3c6d7fe99f6b11487a1b2cdb052b8425d3
Fadhilah Tri Larasaty <fadhilahlaras97@gmail.com>
2021-04-27 11:02:08 +0700

    Delete Class selain Security

--- src/main/java/com/kelompok1/dailyyou/repository/ProductCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kelompok1.dailyyou.repository;
-
-import com.kelompok1.dailyyou.model.entity.ProductCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Integer> {
-}
--- src/main/java/com/kelompok1/dailyyou/service/ProductService.java
+++
@@ -1,6 +0,0 @@
-package com.kelompok1.dailyyou.service;
-
-public interface ProductService {
-
-    Product save(Product product);
-}
