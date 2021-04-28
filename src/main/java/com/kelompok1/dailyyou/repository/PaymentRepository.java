3f195c796061a7767d0d877bcf579fce4d91e36e
Fadhilah Tri Larasaty <fadhilahlaras97@gmail.com>
2021-04-28 21:41:51 +0700

    Add new repository for payment

--- src/main/java/com/kelompok1/dailyyou/repository/PaymentRepository.java
+++ src/main/java/com/kelompok1/dailyyou/repository/PaymentRepository.java
@@ -1,10 +1,10 @@
 package com.kelompok1.dailyyou.repository;
 
-import com.kelompok1.dailyyou.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.kelompok1.dailyyou.model.entity.Users;
 
 @Repository
-public interface PaymentRepository extends JpaRepository<User, Integer> {
+public interface PaymentRepository extends JpaRepository<Users, Integer> {
 
 }
