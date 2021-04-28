b78695f007f52760b616211c804aa67d2d371169
Fadhilah Tri Larasaty <fadhilahlaras97@gmail.com>
2021-04-28 09:51:38 +0700

    Add New Controller for Registration

---
+++ src/main/java/com/kelompok1/dailyyou/controller/mvc/UserRegistrationController.java
@@ -0,0 +1,37 @@
+package com.kelompok1.dailyyou.controller.mvc;
+
+import com.kelompok1.dailyyou.service.UserService;
+import com.kelompok1.dailyyou.model.dto.UserRegistrationDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/signup")
+public class UserRegistrationController {
+
+	private UserService userService;
+
+	public UserRegistrationController(UserService userService) {
+		super();
+		this.userService = userService;
+	}
+	
+	@ModelAttribute("user")
+    public UserRegistrationDto userRegistrationDto() {
+        return new UserRegistrationDto();
+    }
+	
+	@GetMapping
+	public String showRegistrationForm() {
+		return "user/signup";
+	}
+	
+	@PostMapping
+	public String registerUserAccount(@ModelAttribute("user") UserRegistrationDto registrationDto) {
+		userService.save(registrationDto);
+		return "redirect:/signup?success";
+	}
+}
