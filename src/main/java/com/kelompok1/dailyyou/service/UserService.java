66eec28b05ba13b6e07b6cb2f2e32693b0d63288
Fadhilah Tri Larasaty <fadhilahlaras97@gmail.com>
2021-04-28 14:52:15 +0700

    Change name class "users"

--- src/main/java/com/kelompok1/dailyyou/service/UserService.java
+++ src/main/java/com/kelompok1/dailyyou/service/UserService.java
@@ -1,9 +1,9 @@
 package com.kelompok1.dailyyou.service;
 
-import com.kelompok1.dailyyou.model.entity.User;
+import com.kelompok1.dailyyou.model.entity.Users;
 import com.kelompok1.dailyyou.model.dto.UserRegistrationDto;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface UserService extends UserDetailsService {
-    User save(UserRegistrationDto registrationDto);
+    Users save(UserRegistrationDto registrationDto);
 }
