1e5118ee72f002d80334ce37596a7262b022bd24
Fadhilah Tri Larasaty <fadhilahlaras97@gmail.com>
2021-04-28 09:52:41 +0700

    Update Controller

--- src/main/java/com/kelompok1/dailyyou/controller/mvc/UserController.java
+++ src/main/java/com/kelompok1/dailyyou/controller/mvc/UserController.java
@@ -32,52 +32,52 @@ public class UserController {
         model.setViewName("user/login");
         return model;
     }
-
-    @RequestMapping(value= {"/signup"}, method=RequestMethod.GET)
-    public ModelAndView signup() {
-        ModelAndView model = new ModelAndView();
-        User user = new User();
-        model.addObject("user", user);
-        model.setViewName("user/signup");
-
-        return model;
-    }
-
-    @RequestMapping(value= {"/signup"}, method=RequestMethod.POST)
-    public ModelAndView createUser(@Valid User user, BindingResult bindingResult) {
-        ModelAndView model = new ModelAndView();
-        User userExists = userService.findUserByEmail(user.getEmail());
-
-        if(userExists != null) {
-            bindingResult.rejectValue("email", "error.user", "This email already exists!");
-        }
-        if(bindingResult.hasErrors()) {
-            model.setViewName("user/signup");
-        } else {
-            userService.saveUser(user);
-            model.addObject("msg", "User has been registered successfully!");
-            model.addObject("user", new User());
-            model.setViewName("user/signup");
-        }
-
-        return model;
-    }
-
-//    @RequestMapping(value= {"/dashboard"}, method=RequestMethod.GET)
-//    public ModelAndView home() {
-//        ModelAndView model = new ModelAndView();
-//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-//        User user = userService.findUserByEmail(auth.getName());
 //
-//        model.addObject("userName", user.getFirstname() + " " + user.getLastname());
-//        model.setViewName("dashboard/index");
+//    @RequestMapping(value= {"/signup"}, method=RequestMethod.GET)
+//    public ModelAndView signup() {
+//        ModelAndView model = new ModelAndView();
+//        User user = new User();
+//        model.addObject("user", user);
+//        model.setViewName("user/signup");
+//
 //        return model;
 //    }
-
-    @RequestMapping(value= {"/access_denied"}, method=RequestMethod.GET)
-    public ModelAndView accessDenied() {
-        ModelAndView model = new ModelAndView();
-        model.setViewName("error/access_denied");
-        return model;
-    }
+//
+//    @RequestMapping(value= {"/signup"}, method=RequestMethod.POST)
+//    public ModelAndView createUser(@Valid User user, BindingResult bindingResult) {
+//        ModelAndView model = new ModelAndView();
+//        User userExists = userService.findUserByEmail(user.getEmail());
+//
+//        if(userExists != null) {
+//            bindingResult.rejectValue("email", "error.user", "This email already exists!");
+//        }
+//        if(bindingResult.hasErrors()) {
+//            model.setViewName("user/signup");
+//        } else {
+//            userService.saveUser(user);
+//            model.addObject("msg", "User has been registered successfully!");
+//            model.addObject("user", new User());
+//            model.setViewName("user/signup");
+//        }
+//
+//        return model;
+//    }
+//
+////    @RequestMapping(value= {"/dashboard"}, method=RequestMethod.GET)
+////    public ModelAndView home() {
+////        ModelAndView model = new ModelAndView();
+////        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+////        User user = userService.findUserByEmail(auth.getName());
+////
+////        model.addObject("userName", user.getFirstname() + " " + user.getLastname());
+////        model.setViewName("dashboard/index");
+////        return model;
+////    }
+//
+//    @RequestMapping(value= {"/access_denied"}, method=RequestMethod.GET)
+//    public ModelAndView accessDenied() {
+//        ModelAndView model = new ModelAndView();
+//        model.setViewName("error/access_denied");
+//        return model;
+//    }
 }
