fa7cdea8717558eadd622199ff49acc45a76c2db
Fadhilah Tri Larasaty <fadhilahlaras97@gmail.com>
2021-04-29 22:19:01 +0700

    Update antMatchers for activities

--- src/main/java/com/kelompok1/dailyyou/configuration/SecurityConfiguration.java
+++ src/main/java/com/kelompok1/dailyyou/configuration/SecurityConfiguration.java
@@ -49,7 +49,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
                 .antMatchers("/dashboard*").permitAll()
                 .antMatchers("/signup*").permitAll()
                 .antMatchers("/aboutUs*").permitAll()
-                .antMatchers("/activities/*").permitAll()
+                .antMatchers("/educationPillars*").permitAll()
+                .antMatchers("/healthPillars*").permitAll()
+                .antMatchers("/womenPillars*").permitAll()
+                .antMatchers("/environmentPillars*").permitAll()
                 .antMatchers("/dashboard/**").hasAuthority("ROLE_USER")
 //                .antMatchers("/checkout*").hasRole("ROLE_USER")
 
