53fad416599b09971139eb95133b0c7da8b766fa
Khoerul Fatihin <khoerulfatihin1011@gmail.com>
2021-05-01 13:47:06 +0700

    cart js

--- src/main/resources/static/assets/js/cart.js
+++ src/main/resources/static/assets/js/cart.js
@@ -1,142 +1,69 @@
-var tableCart = {
-    create: function () {
-        if ($.fn.DataTable.isDataTable('#tableCart')) {
-            $('#tableCart').DataTable().clear();
-            $('#tableCart').DataTable().destroy();
-        }
-
+var create = {
+    getAllItemCart: function () {
         $.ajax({
-            url: '/api/cart/',
+            url: '/api/cart',
             method: 'get',
             contentType: 'application/json',
             success: function (res, status, xhr) {
                 if (xhr.status == 200 || xhr.status == 201) {
-                    $('#tableCart').DataTable({
-                        data: res,
-                        columns: [
-                            {
-                                title: "product name",
-                                data: "productName"
-                            },
-                            {
-                                title: "product quantity",
-                                data: "productQuantity"
-                            },
-                            {
-                                title: "price",
-                                data: "totalCost"
-                            },
-                            {
-                                title: "Action",
-                                data: null,
-                                render: function (data, type, row) {
-                                    return "<button class='btn-success' data-toggle='tooltip' title='edit' data-placement='bottom' onclick=formCart.setEditData('" + data.idCart + "') style='border-radius: 20%'><i class='fa fa-pencil-alt'></i></button>" + "<span>   </span>" +
-                                        "<button class='btn-danger' data-toggle='tooltip' title='delete' data-placement='bottom' onclick=actionDelete.deleteConfirm('" + data.idCart + "') style='border-radius: 20%'><i class='fa fa-minus-circle'></i></button>"
-                                }
-                            }
-                        ],
-                        scrollX : true
-                    });
-                } else {
+                    console.log(res.length);
+                    var cart = [];
+                    if (res.length > 0 && cart.length < 1) {
+                        for (i = 0; i < res.length; i++) {
+
+                            var number = res[i].id;
+                            var productPictureURL = res[i].pictureUrl;
+                            var productName = res[i].productName;
+                            var productQuantity = res[i].prductQuantity;
+                            var productPrice = res[i].price;
+                            var totalCost = res[i].totalCost;
+                            // console.log(categoryName);
+                            var cartItem = `
+                <tr class="rem1" id="isi">
+                    <td class="invert">1</td>
+                    <td class="invert-image"><a href="single.html"><img src="${productPictureURL}" alt="Product Image " class="img-responsive" /></a></td>
+                    <td class="invert">${productName}</td>
+                    <td class="invert">
+                        <div class="quantity"> 
+                            <div class="quantity-select">                           
+                                <div class="entry value-minus">&nbsp;</div>
+                                <div class="entry value"><span>${productQuantity}</span></div>
+                                <div class="entry value-plus active">&nbsp;</div>
+                            </div>
+                        </div>
+                    </td>
+                    <td class="invert">${productPrice}</td>
+                    <td class="invert">${totalCost}</td>
+                    <td class="invert">
+                    <div class="rem">
+                        <button  class='btn-danger' onclick=deleteCartItem(${number})>Delete</button>
+                    </div>
+                    </td>
+                </tr>
+            `;
+
+                            $("#cart").append(cartItem);
+                        }
+                    } else if (res.length < 1 && cart.length < 1) {
+                        cards.push(errorMessage);
+                    }
                 }
-            },
-            error: function (err) {
-                console.log(err);
             }
-        });
-
-        // $.ajax({
-        //     type: 'get',
-        //     url: '/api/user',
-        //     contentType: 'application/json',
-        //     dataType: 'json',
-        //     success: function (res) {
-        //         console.log(res)
-        //         let opt = '<option disabled selected hidden>-- Pilih --</option>';
-        //         for(let i=0; i<res.length; i++) {
-        //             opt += '<option value=' + res[i].idUser + '>' + res[i].nama + '</option>'
-        //         }
-        //         $('#nama').html(opt);
-        //     }
-        // });
-    }
-};
-
-var formCart = {
-    resetForm: function () {
-        $('#form-cart')[0].reset();
-        $('#idCart').val("");
-    },
-
-    setEditData: function (idCartItem) {
-        formCart.resetForm();
-        $.ajax({
-            url: '/api/cart/update' + idCartItem,
-            method: 'put',
-            contentType: 'application/json',
-            dataType: 'json',
-            success: function (res, status, xhr) {
-                if (xhr.status == 200 || xhr.status == 201) {
-                    $('#form-cart').fromJSON(JSON.stringify(res));
-                    $('#modal-cart').modal('show')
-                } else {
-                }
-            },
-            erorrr: function (err) {
-                console.log(err);
-            }
-        });
-    }
-};
-
-var actionDelete = {
-    // deleteConfirm: function (id) {
-    //     $.ajax({
-    //         url: '/api/cart/delete' + id,
-    //         method: 'get',
-    //         contentType: 'application/json',
-    //         dataType: 'json',
-    //         success: function (res, status, xhr) {
-    //             if (xhr.status == 200 || xhr.status == 201) {
-    //                 $('#form-transaksi').fromJSON(JSON.stringify(res));
-    //                 $('#modal-delete').modal('show')
-    //             } else {
-    //
-    //             }
-    //         },
-    //         erorrr: function (err) {
-    //             console.log(err);
-    //         }
-    //     });
-    // },
-
-    deleteRowData : function () {
-        if ($('#form-cart').parsley().validate()) {
-            var dataResult = getJsonForm($("#form-cart").serializeArray(), true);
-
-            $.ajax({
-                url: '/api/cart/delete' + dataResult.idCart,
-                method: 'delete',
-                success: function () {
-                    tableCart.create();
-                    $('#modal-delete').modal('hide');
-                },
-                erorrr: function (err) {
-                    console.log(err);
-                }
-            });
-        }
-    },
-
-    deleteTable : function () {
-        $.ajax({
-            url: '/api/cart',
-            method: 'delete',
-            success: function () {
-                tableCart.create()
-                $('#modal-delete').modal('hide')
-            }
-
         })
-    }
-};
+    },
+}
+
+$("#cart").on("click", "#btn-danger", function () {
+    $.ajax({
+        url: '/api/cart/delete{id}',
+        method: 'delete',
+        contentType: 'application/json',
+        success: function (res, status, xhr) {
+            if (xhr.status == 200 || xhr.status == 201) {
+                console.log(res);
+                alert("Thank you for trusting Daily You!");
+            }
+        }
+    })
+
+});
