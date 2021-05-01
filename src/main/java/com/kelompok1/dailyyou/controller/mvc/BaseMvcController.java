9d127fb21f10b9ffa3a9ab354182f92cdbd327d2
Khoerul Fatihin <khoerulfatihin1011@gmail.com>
2021-05-01 14:58:03 +0700

    update js

--- src/main/java/com/kelompok1/dailyyou/controller/mvc/BaseMvcController.java
+++ src/main/java/com/kelompok1/dailyyou/controller/mvc/BaseMvcController.java
@@ -48,4 +48,5 @@ public class BaseMvcController {
     public String skinCare() {
         return "product/skincare";
     }
+
 }
---
+++ src/main/java/com/kelompok1/dailyyou/controller/mvc/CartController.java
@@ -0,0 +1,15 @@
+package com.kelompok1.dailyyou.controller.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/")
+public class CartController {
+    //    dashboard
+    @GetMapping("cart")
+    public String cartItem() {
+        return "checkout/checkout";
+    }
+};
