cb72a1aed0d4274edc33739ab41e66ffbb1b9bae
Fadhilah Tri Larasaty <fadhilahlaras97@gmail.com>
2021-05-02 22:18:19 +0700

    Add annotation @Data to change setter getter manual

--- src/main/java/com/kelompok1/dailyyou/controller/restapi/ApiUser.java
+++ src/main/java/com/kelompok1/dailyyou/controller/restapi/ApiUser.java
@@ -1,5 +1,8 @@
 package com.kelompok1.dailyyou.controller.restapi;
 
+import lombok.Data;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -10,15 +13,12 @@ import com.kelompok1.dailyyou.model.dto.UserRegistrationDto;
 import com.kelompok1.dailyyou.service.UserService;
 
 @Controller
+@Data
 @RequestMapping("/signup")
 public class ApiUser {
 
+    @Autowired
     private final UserService userService;
-
-    public ApiUser(UserService userService) {
-        super();
-        this.userService = userService;
-    }
 
     @ModelAttribute("user")
     public UserRegistrationDto userRegistrationDto() {
@@ -33,6 +33,6 @@ public class ApiUser {
     @PostMapping
     public String registerUserAccount(@ModelAttribute("user") UserRegistrationDto registrationDto) {
         userService.save(registrationDto);
-        return "redirect:/registration?success";
+        return "redirect:/signup?success";
     }
 }
