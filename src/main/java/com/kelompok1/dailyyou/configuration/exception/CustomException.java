c7b62b90ad8f72be6d5dbe7d3a069dea80b1235f
Fadhilah Tri Larasaty <fadhilahlaras97@gmail.com>
2021-05-02 22:19:08 +0700

    Change exception

---
+++ src/main/java/com/kelompok1/dailyyou/configuration/exception/CustomException.java
@@ -0,0 +1,7 @@
+package com.kelompok1.dailyyou.configuration.exception;
+
+public class CustomException extends IllegalArgumentException {
+    public CustomException(String msg) {
+        super(msg);
+    }
+}
