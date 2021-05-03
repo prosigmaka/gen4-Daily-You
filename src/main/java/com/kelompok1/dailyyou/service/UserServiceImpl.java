e233894233b0a8ed06a2d2a93d3350c4b64d8f46
Fadhilah Tri Larasaty <fadhilahlaras97@gmail.com>
2021-05-03 13:41:26 +0700

    Update script user service

--- src/main/java/com/kelompok1/dailyyou/service/UserServiceImpl.java
+++ src/main/java/com/kelompok1/dailyyou/service/UserServiceImpl.java
@@ -4,66 +4,63 @@ import com.kelompok1.dailyyou.model.entity.Users;
 import com.kelompok1.dailyyou.model.entity.Role;
 import com.kelompok1.dailyyou.repository.UserRepository;
 import com.kelompok1.dailyyou.model.dto.UserRegistrationDto;
+import com.kelompok1.dailyyou.configuration.exception.CustomException;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.stream.Collectors;
 
 @Service("userService")
 public class UserServiceImpl implements UserService{
 
+    @Autowired
     private UserRepository userRepository;
 
     @Autowired
-    private BCryptPasswordEncoder passwordEncoder;
+    private PasswordEncoder passwordEncoder;
 
-    @Autowired
-    private ModelMapper modelMapper;
 
-    public UserServiceImpl(UserRepository userRepository) {
-        super();
-        this.userRepository = userRepository;
-    }
-
+    //Untuk proses save data saat proses registrasi
     @Override
-    public Users save(UserRegistrationDto registrationDto) {
-//        Users users = new Users(registrationDto.getFirstName(),
-//                registrationDto.getLastName(), registrationDto.getEmail(),
-//                passwordEncoder.encode(registrationDto.getPassword()), Arrays.asList(new Role("ROLE_USER")));
+    public Users save(UserRegistrationDto registrationDto) throws CustomException {
 
-        //		Role role = roleRepository.findById(1L).get();
+        //Setting manual
         Users users = new Users();
-
-//        Users users = modelMapper.map(UserRegistrationDto, Users.class);
-
         users.setFirstName(registrationDto.getFirstName());
         users.setLastName(registrationDto.getLastName());
-        users.setEmail(registrationDto.getEmail());
+        users.setUsername(registrationDto.getUsername());
         users.setPassword(passwordEncoder.encode(registrationDto.getPassword()));
+
+        //Setting manual untuk role id 1 dengan tipe data Long adalah sebagai "User"
         users.setRoleId(1L);
 
         return userRepository.save(users);
-
     }
 
+    // Proses Login dimana akan dicari usernamenya apakah sesuai apa tidak
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        Users users = userRepository.findByEmail(username);
+        Users users = userRepository.findByUsername(username);
+
+        //Jika usernya tidak ada di database atau null
         if(users == null) {
+            //maka akan akan dilemparkan ke "UsernameNotFoundException"
             throw new UsernameNotFoundException("Invalid username or password.");
+//            users loginDto = new LoginDto();
+//            loginDto.getIdUserLogin();
         }
-        return new org.springframework.security.core.userdetails.User(users.getEmail(), users.getPassword(), mapRolesToAuthorities(Arrays.asList(users.getRoles())));
+
+        //Setelah itu akan di return nilainya ke user.
+        return new org.springframework.security.core.userdetails.User(users.getUsername(), users.getPassword(), mapRolesToAuthorities(Arrays.asList(users.getRoles())));
     }
 
     private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles){
