097ffe70f2942339808e032fc98943db8b9562a1
Fadhilah Tri Larasaty <fadhilahlaras97@gmail.com>
2021-05-04 09:51:01 +0700

    Update entity user

--- src/main/java/com/kelompok1/dailyyou/model/entity/Users.java
+++ src/main/java/com/kelompok1/dailyyou/model/entity/Users.java
@@ -27,8 +27,8 @@ public class Users {
     @Column(name = "username", nullable = false, unique = true)
     private String username;
 
-//    @Column(name="email", nullable = false, unique = true)
-//    private String email;
+    @Column(name="email", nullable = false, unique = true)
+    private String email;
 
     @Column(name="password", nullable = false)
     private String password;
