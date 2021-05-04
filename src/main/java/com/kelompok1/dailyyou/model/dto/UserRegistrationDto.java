54a1ee57b617b9b310d5d4cbe6fe51b5708e844d
Fadhilah Tri Larasaty <fadhilahlaras97@gmail.com>
2021-05-04 09:51:29 +0700

    Update dto user

--- src/main/java/com/kelompok1/dailyyou/model/dto/UserRegistrationDto.java
+++ src/main/java/com/kelompok1/dailyyou/model/dto/UserRegistrationDto.java
@@ -8,5 +8,6 @@ public class UserRegistrationDto {
     private String firstName;
     private String lastName;
     private String username;
+    private String email;
     private String password;
 }
