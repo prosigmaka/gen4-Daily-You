a580c80f584ddeb44e9cc87614529f61728314f2
Fadhilah Tri Larasaty <fadhilahlaras97@gmail.com>
2021-05-07 23:30:18 +0700

    Delete

--- src/main/java/com/kelompok1/dailyyou/service/CartService.java
+++
@@ -1,9 +0,0 @@
-package com.kelompok1.dailyyou.service;
-
-import com.kelompok1.dailyyou.model.entity.Cart;
-
-public interface CartService {
-
-    Cart save(Cart cart);
-}
-
--- src/main/java/com/kelompok1/dailyyou/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.kelompok1.dailyyou.service;
-
-import com.kelompok1.dailyyou.model.entity.Users;
-import com.kelompok1.dailyyou.model.dto.UserRegistrationDto;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserService extends UserDetailsService {
-    Users save(UserRegistrationDto registrationDto);
-}
